e8e4d491118f5be19382c58733051dbd90e5536e
lamo2k123 <lamo2k123@gmail.com>
2017-02-28 23:19:10 +0300

    added history action

--- src/browser-router.tsx
+++ src/browser-router.tsx
@@ -66,7 +66,7 @@ export default class BrowserRouter extends Router {
     go(n) {
         this.history.go(n);
     }
-    private _locationChanged = async ({ pathname, hash, search }) => {
+    private _locationChanged = async ({ pathname, hash, search }, action) => {
         const path = pathname + search + hash;
         let { location, route, status, params, redirect, result, ctx, error } = await this.router.run({ path });
         if (error !== null) {
@@ -84,7 +84,7 @@ export default class BrowserRouter extends Router {
                 error
             }
         };
-        const renderCallback = Router.makeCallback(this.router, { path, location, route, status, params, redirect, result, ctx });
+        const renderCallback = Router.makeCallback(this.router, { path, location, route, status, params, redirect, result, action, ctx });
         this.changeComponent({ Component: result, componentProps: props, path, location, error, renderCallback });
     };
     componentDidMount() {
