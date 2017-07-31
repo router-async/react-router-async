ecb5b844132829550304827f1b7d2c91f152c24f
olegman <olegman.ru@gmail.com>
2017-07-31 15:23:23 +0300

    prevent XSS

--- src/server-router.tsx
+++ src/server-router.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
 import Router, { initParams, initResult } from './router';
+import serialize from 'serialize-javascript';
 
 export default class ServerRouter extends Router {
     static async init(opts: initParams): Promise<initResult> {
@@ -22,9 +23,9 @@ export default class ServerRouter extends Router {
         return (
             <div>
                 {this.props.children ? this.props.children : <this.state.Component {...this.state.componentProps} />}
-                <script dangerouslySetInnerHTML={{ __html: `window.__REACT_ROUTER_ASYNC__=${JSON.stringify({
+                <script dangerouslySetInnerHTML={{ __html: `window.__REACT_ROUTER_ASYNC__=${serialize({
                     state: this.state
-                })};`}} />
+                }, {isJSON: true})};`}} />
             </div>
         )
     }
